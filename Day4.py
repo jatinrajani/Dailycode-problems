02913aa7ca30c9285dae12e47a6ca1865e39977b
Jatin Rajani <jatinrajani@users.noreply.github.com>
2019-05-14 14:19:00 +0530

    Stripe Interview Problem
    Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
    
    For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
    
    You can modify the input array in-place.

---
+++ Day4.py
@@ -0,0 +1,12 @@
+#
+def firspositive(a):
+    a.sort()
+    i=a[0]
+    j=0
+    while i<=a[len(a)-1]:
+          if i in a:
+              i+=1
+          else:
+              return [i]    
+    if i>a[len(a)-1]:
+       return i
